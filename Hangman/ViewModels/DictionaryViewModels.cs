ad6c4cd2a491ab12991810cad48151e58a55799a
Piterdv <piotrludwinski@hotmail.com>
2023-11-15 11:05:41 +0100

    Revert "Ustaw możliwośc wyboru na lokalny słownik tylko dla sytuacji kiedy klikamy na przycisk wyboru słownika."
    This reverts commit 8d68d6b66940384d608943cc44be55c6ce3ae6d8.

--- Hangman/ViewModels/DictionaryViewModels.cs
+++ Hangman/ViewModels/DictionaryViewModels.cs
@@ -289,7 +289,7 @@ namespace Hangman.ViewModels
         private void Close(object obj)
         {
             if (_wordEntities != null && _wordEntities.Count > 0) _wordEntities.RemoveAt(0);
-            if(_mvm != null) LocalDictionary.Dictionary = _wordEntities;
+            LocalDictionary.Dictionary = _wordEntities;
             (obj as Window)?.Close();
         }
 
